74a1d8d058ca47a6529db324cab32925744be72a
OlhaMajerski <olha1991o@gmail.com>
2024-05-09 13:32:10 +0200

    task2

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,28 @@
-class Storage 
+class Storage {
+ #items = [];
+  getItems() {
+    return this.#items;
+}
+ addItem(newItem) {
+    this.#items.push(newItem);
+    return this.#items.length;
+}
+ removeItem(itemToRemove){
+    this.#items = this.#items.filter(item => item !== itemToRemove);
+}
+constructor(params){
+    this.#items = params;
+}
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
